ff80b7ce134a421f65b1a5505c93de83ed7aa232
Konrad Klimczak <konrad.klimczak.4@gmail.com>
2024-08-09 12:27:43 +0200

    fix(products): add transformer for decimal type

--- backend/src/entities/product.entity.js
+++ backend/src/entities/product.entity.js
@@ -1,5 +1,16 @@
 const { EntitySchema } = require("typeorm");
 
+// Decimal type in MariaDB need additional transformer to convert string to number
+// due to the fact that MariaDB return decimal as string
+class ColumnNumericTransformer {
+  to(data) {
+    return data;
+  }
+  from(data) {
+    return parseFloat(data);
+  }
+}
+
 const ProductEntity = new EntitySchema({
   name: "Product",
   tableName: "products",
@@ -14,6 +25,9 @@ const ProductEntity = new EntitySchema({
     },
     price: {
       type: "decimal",
+      transformer: new ColumnNumericTransformer(),
+      precision: 5,
+      scale: 2,
     },
     quantity: {
       type: "int",
